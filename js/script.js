27a441ebc33e340bdf2093d979740a79b9563db7
Itsrajsk <rajkashid146303@gmail.com>
2024-03-30 23:26:16 +0530

    Added Login Form

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
-let searchForm = document.querySelector('.search-form');
+// Search Button
+let searchForm = document.querySelector(".search-form");
 
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-}
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+};
+// Add To Cart Button
+let shoppingCart = document.querySelector(".shopping-cart");
 
-let shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector("#cart-btn").onclick = () => {
+  shoppingCart.classList.toggle("active");
+};
+// Login Form Button
+let loginForm = document.querySelector(".login-form");
 
-document.querySelector('#cart-btn').onclick = () =>{
-    shoppingCart.classList.toggle('active');
-}
+document.querySelector("#login-btn").onclick = () => {
+  loginForm.classList.toggle("active");
+};
